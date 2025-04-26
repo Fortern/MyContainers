27392a10e8d53f11399d5a96b58a6bbe3eb0c1d3
Fortern <blueten.ki@gmail.com>
2025-04-26 08:00:57 +0800

    update

--- LeavesMC/README.md
+++ LeavesMC/README.md
@@ -4,7 +4,7 @@ sudo docker build -t leavesmc/minecraft-leaves-server:1.21.4-33 --build-arg URL=
 ```
 
 ## run
-This data directory needs to be writable by the Leaves process, which runs as UID 25565 and GID 25565.
+默认以 UID 25565 和 GID 25565 运行 Java 进程。如果你指定了 --user 参数，则需要你手动处理 data 目录的权限。
 ```shell
 sudo mkdir /srv/leaves && sudo chown -R 25565:25565 /srv/leaves
 sudo docker run -dit --name leaves \
--- LeavesMC/run_leaves.sh
+++ LeavesMC/run_leaves.sh
@@ -1,2 +1,22 @@
-#!/bin/sh
-exec java -jar -Xms$MEMORY_MIN -Xmx$MEMORY_MAX $JAVAFLAGS /opt/leaves/leaves.jar nogui
+#!/bin/bash
+
+set -e
+
+if [ "$(id -u)" -eq 0 ]; then
+  LEAVES_USER='leaves'
+  LEAVES_GROUP='leaves'
+  
+  if ! id "$LEAVES_USER" >/dev/null 2>&1; then
+    groupadd --system --gid 25565 $LEAVES_USER
+    useradd --system --uid 25565 --gid 25565 --home-dir /opt/leaves --no-create-home $LEAVES_GROUP
+  fi
+
+  chown -R $LEAVES_USER:$LEAVES_GROUP /data /opt/leaves
+  echo "running as uid $LEAVES_USER:$LEAVES_GROUP"
+  JAVA_CMD="gosu $LEAVES_USER:$LEAVES_GROUP java"
+else
+  echo "running as uid $(id -u)"
+  JAVA_CMD="java"
+fi
+
+exec $JAVA_CMD -jar -Xms$MEMORY_MIN -Xmx$MEMORY_MAX $JAVAFLAGS /opt/leaves/leaves.jar nogui
